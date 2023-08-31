cd5d4910e026aeb3161355251c089dc48dba603e
WeraGitHub <weronikalimberger@gmail.com>
2023-08-31 09:41:21 +0100

    Add route for a question

--- application/routes.py
+++ application/routes.py
@@ -10,6 +10,12 @@ def home():
     return render_template('home.html', title=title_name)
 
 
+@app.route('/question/<category>/<q_number>')   #  http://127.0.0.1:5000/question/aws/2
+def question(category, q_number):
+    title_name = "Quizlet"
+    return render_template('question.html', title=title_name, category=category, q_number=q_number)
+
+
 @app.route('/question_list')
 def question_list():
     return create_html_question_list()
