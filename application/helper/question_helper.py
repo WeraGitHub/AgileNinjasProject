29ee5990a9bd133de3993784709de18fecd0a792
James Guest <james.guest@sky.uk>
2023-09-05 10:38:28 +0100

    ammended db connection to work on production

--- application/helper/question_helper.py
+++ application/helper/question_helper.py
@@ -1,6 +1,8 @@
 # Import necessary modules and classes
 from application import mysql  # Import the 'mysql' object from the 'application' module
-from application.objects.question import Question  # Import the 'Question' class from the 'application.objects.question' module
+from application.objects.question import \
+    Question  # Import the 'Question' class from the 'application.objects.question' module
+
 
 # Define a function to retrieve questions from a specific category in the database
 def get_questions_from_category_from_db(category):
@@ -21,6 +23,7 @@ def get_questions_from_category_from_db(category):
     # Return the list of questions
     return question_list
 
+
 # Define a function to retrieve all questions from the database
 def get_all_questions_from_db():
     # Create a cursor object to interact with the database
@@ -40,6 +43,7 @@ def get_all_questions_from_db():
     # Return the list of questions
     return question_list
 
+
 # Define a function to add a new question to the database
 def add_question_to_db(question, answer, category):
     # Establish a database connection
@@ -49,10 +53,12 @@ def add_question_to_db(question, answer, category):
     cursor = connection.cursor()
 
     # Execute a SQL query to insert a new row into the 'questions' table with the provided question, answer, and category
-    cursor.execute('INSERT INTO questions (question, answer, category) VALUES (%s, %s, %s)', (question, answer, category))
+    cursor.execute('INSERT INTO questions (question, answer, category) VALUES (%s, %s, %s)',
+                   (question, answer, category))
 
     # Commit the changes to the database
     connection.commit()
+
 
 # Define a function to update an existing question in the database
 def update_question_in_db(id, question, answer, category):
@@ -63,10 +69,12 @@ def update_question_in_db(id, question, answer, category):
     cursor = connection.cursor()
 
     # Execute a SQL query to update the 'questions' table with the provided question, answer, and category where the 'id' matches
-    cursor.execute('UPDATE questions SET question=%s, answer=%s, category=%s WHERE id=%s', (question, answer, category, id))
+    cursor.execute('UPDATE questions SET question=%s, answer=%s, category=%s WHERE id=%s',
+                   (question, answer, category, id))
 
     # Commit the changes to the database
     connection.commit()
+
 
 # Define a function to delete a question from the database by its ID
 def delete_question_from_db(question_id):
