4e6e73d2b03ab1caaa59340e170ce48deb6a0e27
WeraGitHub <weronikalimberger@gmail.com>
2023-10-18 10:00:03 +0100

    Format terraform files (white space)

--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ variable "project" {
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
@@ -21,17 +21,17 @@ terraform {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = var.aws_region
+  region     = var.aws_region
 }
 
 
 # VPC
 resource "aws_vpc" "agile_ninjas_VPC" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "agile-ninjas_vpc"
+    Name    = "agile-ninjas_vpc"
     project = var.project
   }
 }
@@ -40,52 +40,52 @@ resource "aws_vpc" "agile_ninjas_VPC" {
 # Subnets
 
 resource "aws_subnet" "private_subnet-a" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "eu-west-2a"
+  vpc_id                  = aws_vpc.agile_ninjas_VPC.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "private_subnet-b" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-west-2b"
+  vpc_id                  = aws_vpc.agile_ninjas_VPC.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2b"
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "private_subnet-c" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "eu-west-2c"
+  vpc_id                  = aws_vpc.agile_ninjas_VPC.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-west-2c"
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "public_subnet-a" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "eu-west-2a"
+  vpc_id                  = aws_vpc.agile_ninjas_VPC.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet-b" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  cidr_block = "10.0.4.0/24"
-  availability_zone = "eu-west-2b"
+  vpc_id                  = aws_vpc.agile_ninjas_VPC.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "eu-west-2b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet-c" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  cidr_block = "10.0.5.0/24"
-  availability_zone = "eu-west-2c"
+  vpc_id                  = aws_vpc.agile_ninjas_VPC.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "eu-west-2c"
   map_public_ip_on_launch = true
 }
 
 # Subnet group
 resource "aws_db_subnet_group" "private_subnet_group" {
-  name = "private_subnet_group"
+  name        = "private_subnet_group"
   description = "Private subnets group for our db"
-  subnet_ids = [aws_subnet.private_subnet-a.id, aws_subnet.private_subnet-b.id, aws_subnet.private_subnet-c.id]
+  subnet_ids  = [aws_subnet.private_subnet-a.id, aws_subnet.private_subnet-b.id, aws_subnet.private_subnet-c.id]
 }
 
 
@@ -101,7 +101,7 @@ resource "aws_eip" "nat_gateway_eip" {
 ## NAT gateway
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id = aws_subnet.public_subnet-a.id
+  subnet_id     = aws_subnet.public_subnet-a.id
 
   tags = {
     Name = "Nat gateway"
@@ -114,7 +114,7 @@ resource "aws_route_table" "public_route_table" {
 
   route {
     cidr_block = "10.0.0.0/16"
-    gateway_id = "local"  # Route traffic within the VPC locally
+    gateway_id = "local" # Route traffic within the VPC locally
   }
 
   route {
@@ -149,11 +149,11 @@ resource "aws_route_table" "private_route_table" {
 
   route {
     cidr_block = "10.0.0.0/16"
-    gateway_id = "local"  # Route traffic within the VPC locally
+    gateway_id = "local" # Route traffic within the VPC locally
   }
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
   tags = {
@@ -182,35 +182,35 @@ resource "aws_route_table_association" "private_subnet_association_c" {
 # Security groups
 
 resource "aws_security_group" "rds_sg" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  name = "rds_sg"
+  vpc_id      = aws_vpc.agile_ninjas_VPC.id
+  name        = "rds_sg"
   description = "RDS Security Group"
   # Inbound: Allows MySQL (3306) traffic only from the EC2 instances' security group.
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     security_groups = [aws_security_group.ec2_sg.id]
   }
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     security_groups = [aws_security_group.pipe_sg.id]
   }
   # Outbound: Allows all outbound traffic to the EC2 instances' security group.
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
     security_groups = [aws_security_group.ec2_sg.id]
   }
 }
 
 resource "aws_security_group" "ec2_sg" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  name = "ec2_sg"
+  vpc_id      = aws_vpc.agile_ninjas_VPC.id
+  name        = "ec2_sg"
   description = "EC2 Security Group - allow HTTP traffic from ELB security group"
   # Inbound: Allows HTTP (80) traffic only from the Load Balancer's security group.
   ingress {
@@ -229,14 +229,14 @@ resource "aws_security_group" "ec2_sg" {
 }
 
 resource "aws_security_group" "lb_sg" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  name = "lb_sg"
+  vpc_id      = aws_vpc.agile_ninjas_VPC.id
+  name        = "lb_sg"
   description = "Load Balancer Security Group - allow incoming HTTP traffic from the internet"
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from anywhere
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from anywhere
   }
   egress {
     from_port   = 0
@@ -247,22 +247,22 @@ resource "aws_security_group" "lb_sg" {
 }
 
 resource "aws_security_group" "pipe_sg" {
-  vpc_id = aws_vpc.agile_ninjas_VPC.id
-  name = "pipe_sg"
+  vpc_id      = aws_vpc.agile_ninjas_VPC.id
+  name        = "pipe_sg"
   description = "Pipeline EC2 Security Group - allow HTTP, ssh and tcp 8080 for Jenkins"
   # Inbound: Allows HTTP (80) traffic from anywhere
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # this should be set to your own and trusted IP address
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # this should be set to your own and trusted IP address
   }
   # for Jenkins
   ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # this should be set to your own and trusted IP address
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # this should be set to your own and trusted IP address
   }
   # for Grafana connection
   ingress {
@@ -297,17 +297,17 @@ resource "aws_security_group" "pipe_sg" {
 
 # RDS
 resource "aws_db_instance" "agile-ninjas-rds-db" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "8.0.33"
-  instance_class       = "db.t3.micro"
-  db_name              = "agile_ninjas"
-  username             = "root"
-  password             = var.rds_password
-  db_subnet_group_name = aws_db_subnet_group.private_subnet_group.name
-  multi_az             = true # Enable multi-AZ deployment
-  skip_final_snapshot  = true
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0.33"
+  instance_class         = "db.t3.micro"
+  db_name                = "agile_ninjas"
+  username               = "root"
+  password               = var.rds_password
+  db_subnet_group_name   = aws_db_subnet_group.private_subnet_group.name
+  multi_az               = true # Enable multi-AZ deployment
+  skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id, aws_security_group.pipe_sg.id]
 }
 
@@ -319,16 +319,16 @@ data "aws_db_instance" "data-rds" {
 
 # EC2 template
 resource "aws_launch_template" "web-app-template" {
-  name_prefix   = "web-app-lt-"
-  image_id      = "ami-028eb925545f314d6"
-  instance_type = "t2.micro"
+  name_prefix            = "web-app-lt-"
+  image_id               = "ami-028eb925545f314d6"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   user_data = base64encode(templatefile("${path.module}/init.tpl",
     {
       # remove port number in case it's attached to the endpoint
-      db_endpoint   = replace(data.aws_db_instance.data-rds.endpoint, ":3306", ""),
-      rds_user      = var.rds_user,
-      rds_password  = var.rds_password,
+      db_endpoint  = replace(data.aws_db_instance.data-rds.endpoint, ":3306", ""),
+      rds_user     = var.rds_user,
+      rds_password = var.rds_password,
     }
   ))
   # Specify the dependency on the pipeline-ec2 - we need our table in RDS to be initialised thanks to the user_data script in the pipe-ec2
@@ -338,18 +338,18 @@ resource "aws_launch_template" "web-app-template" {
 
 # Auto Scaling Group
 resource "aws_autoscaling_group" "auto-scaling-group" {
-  name                 = "autogroup"
-#  name = "AgileNinjas-ASG"
+  name = "autogroup"
+  #  name = "AgileNinjas-ASG"
   launch_template {
-    id = aws_launch_template.web-app-template.id
-    version = "$Latest"  # You can specify a specific version if needed
+    id      = aws_launch_template.web-app-template.id
+    version = "$Latest" # You can specify a specific version if needed
   }
-  min_size             = 1
-  desired_capacity     = 1
-  max_size             = 1
-  vpc_zone_identifier  = [aws_subnet.private_subnet-a.id, aws_subnet.private_subnet-b.id, aws_subnet.private_subnet-c.id]
-  health_check_type    = "ELB"
-  health_check_grace_period = 300  # 5 minutes grace period
+  min_size                  = 1
+  desired_capacity          = 1
+  max_size                  = 1
+  vpc_zone_identifier       = [aws_subnet.private_subnet-a.id, aws_subnet.private_subnet-b.id, aws_subnet.private_subnet-c.id]
+  health_check_type         = "ELB"
+  health_check_grace_period = 300 # 5 minutes grace period
   # Attach the ASG to the ALB target group
   target_group_arns = [aws_lb_target_group.lb-target-group.arn]
 }
@@ -358,10 +358,10 @@ resource "aws_autoscaling_group" "auto-scaling-group" {
 # Application Load Balancer
 resource "aws_lb" "app-load-balancer" {
   name = "app-load-balancer"
-#  internal = false
+  #  internal = false
   load_balancer_type = "application"
-  subnets = [aws_subnet.public_subnet-a.id, aws_subnet.public_subnet-b.id, aws_subnet.public_subnet-c.id]
-  security_groups = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.public_subnet-a.id, aws_subnet.public_subnet-b.id, aws_subnet.public_subnet-c.id]
+  security_groups    = [aws_security_group.lb_sg.id]
 }
 
 # Load balancer target group
@@ -387,16 +387,16 @@ resource "aws_lb_listener" "load-balancer-listener" {
 
 # Pipeline EC2
 resource "aws_instance" "pipeline-ec2" {
-  subnet_id = aws_subnet.public_subnet-b.id
-  ami      = "ami-028eb925545f314d6"
-  instance_type = "t2.medium"
+  subnet_id              = aws_subnet.public_subnet-b.id
+  ami                    = "ami-028eb925545f314d6"
+  instance_type          = "t2.medium"
   vpc_security_group_ids = [aws_security_group.pipe_sg.id]
   user_data = base64encode(templatefile("${path.module}/pipeinit.tpl",
     {
-      db_endpoint   = replace(data.aws_db_instance.data-rds.endpoint, ":3306", ""),
-      rds_user      = var.rds_user,
-      rds_password  = var.rds_password,
-      lb_dns_name = aws_lb.app-load-balancer.dns_name,
+      db_endpoint  = replace(data.aws_db_instance.data-rds.endpoint, ":3306", ""),
+      rds_user     = var.rds_user,
+      rds_password = var.rds_password,
+      lb_dns_name  = aws_lb.app-load-balancer.dns_name,
     }
   ))
   tags = {
