a4b61e181d44641cfc063df21ef450996803444f
Weronika <weronikalimberger@gmail.com>
2023-11-13 14:43:04 +0000

    Remove comment

--- infra/modules/network/nat_gateway/main.tf
+++ infra/modules/network/nat_gateway/main.tf
@@ -10,7 +10,6 @@ resource "aws_eip" "nat_gateway_eip" {
 ## NAT gateway
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_eip.id
-  #  subnet_id     = aws_subnet.public_subnet_a.id
   subnet_id = var.subnet_id
   tags = {
     Name = "agile_ninjas_nat_gateway"
